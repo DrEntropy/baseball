6b72bb4b9685dbb70fb133ea3c11c76624a5fe10
DrEntropy <DrEntropy@users.noreply.github.com>
2025-05-20 17:05:34 -0700

    add exercise 4

--- ABDWRChapter6.R
+++ ABDWRChapter6.R
@@ -97,8 +97,103 @@ count_plot
 
 # Investigate path dependancy...
 
+
+###############
+# Umpire ()
 # no need to load the data is in the package!
 
 data("umpires")
-data("cabrera")
-data("verlander")
+sample_n(umpires,10)
+
+pred_area <- expand_grid(
+  px = seq(-2, 2, by = 0.1),
+  pz = seq(0, 6, by = 0.1)
+)
+
+umpires_rhb <- umpires |>
+  filter(
+    batter_hand == "R",
+    balls == 0 & strikes == 0 |
+      balls == 3 & strikes == 0 |
+      balls == 0 & strikes == 2
+  )
+
+ump_counts <- umpires_rhb |>
+  mutate(count = paste(balls, strikes, sep = "-")) |>
+  group_by(count)
+
+counts <- ump_counts |>
+  group_keys() |>
+  pull(count)
+
+ump_count_fits <- ump_counts |>
+  group_split() |>
+  map(sample_n, 3000) |>
+  map(~loess(
+    called_strike ~ px + pz, data = .x,
+    control = loess.control(surface = "direct"))
+  ) |>
+  map(predict, newdata = pred_area) |>
+  map(~tibble(pred_area, fit = as.numeric(.x))) |>
+  set_names(nm = counts) |>
+  list_rbind(names_to = "count") |>
+  mutate(
+    balls = str_sub(count, 1, 1),
+    strikes = str_sub(count, 3, 3)
+  )
+
+k_zone_plot <-   ggplot(mapping = aes(x = px, y = pz)) +
+  geom_rect(
+    xmin = -0.947, xmax = 0.947, ymin = 1.5,
+    ymax = 3.6, fill = "lightgray", alpha = 0.01
+  ) +
+  coord_equal() +
+  scale_x_continuous(
+    "Horizontal location (ft.)",
+    limits = c(-2, 2)
+  ) +
+  scale_y_continuous(
+    "Vertical location (ft.)",
+    limits = c(0, 5)
+  )
+
+
+crc_fc = c( "#2905a1" ,"#e41a1c", "#4daf4a", "#984ea3")
+k_zone_plot %+% filter(ump_count_fits, fit < 0.6 & fit > 0.4) +
+  geom_contour(
+    aes(z = fit, color = count, linetype = count),
+    binwidth = 0.1
+  ) +  scale_color_manual(values = crc_fc)
+
+###########
+#. Excersize 4
+
+# Umpire’s Strike Zone
+
+#By drawing a contour plot, compare the umpire’s strike
+#zone for left-handed and right-handed batters.
+#Use only the rows of the data frame where the
+#pitch type is a four-seam fastball.
+
+umpire_ff = umpires |> filter(pitch_type == "FF") |>
+  group_by(batter_hand)
+
+batter_hands = umpire_ff |> group_keys() |> pull(batter_hand)
+ump_hand_fits <- umpire_ff   |>
+  group_split() |>
+  map(sample_n, 3000) |>
+  map(~loess(
+    called_strike ~ px + pz, data = .x,
+    control = loess.control(surface = "direct"))
+  ) |>
+  map(predict, newdata = pred_area) |>
+  map(~tibble(pred_area, fit = as.numeric(.x))) |>
+  set_names(nm = batter_hands ) |>
+  list_rbind(names_to = "batter_hand")
+
+k_zone_plot %+% filter(ump_hand_fits, fit < 0.6 & fit > 0.4) +
+  geom_contour(
+    aes(z = fit, color = batter_hand, linetype = batter_hand),
+    binwidth = 0.1
+  ) +  scale_color_manual(values = crc_fc)
+
